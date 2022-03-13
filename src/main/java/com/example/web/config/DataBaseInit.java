d15da82142c45e27a07c4548de63e51542ff0f20
User <faradzhev1996@mail.ru>
2022-03-14 00:47:29 +0700

    First commit

---
+++ src/main/java/com/example/web/config/DataBaseInit.java
@@ -0,0 +1,41 @@
+package com.example.web.config;
+
+import com.example.web.model.Role;
+import com.example.web.model.User;
+import com.example.web.service.RoleService;
+import com.example.web.service.UserService;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class DataBaseInit {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public DataBaseInit(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    private void startDB() {
+        User user = new User("user", "Maxim", "Lavrov","user");
+        User admin = new User("admin", "Viktor", "Barinov", "admin");
+        User moderator = new User("moderator", "Dmitry", "Nagiev", "moderator");
+        Role userRole = new Role("ROLE_USER");
+        Role adminRole = new Role("ROLE_ADMIN");
+        Set<Role> moderatorRole = new HashSet<>();
+        moderatorRole.add(adminRole);
+        moderatorRole.add(userRole);
+        roleService.addRole(userRole);
+        roleService.addRole(adminRole);
+        user.setOneRole(userRole);
+        admin.setOneRole(adminRole);
+        moderator.setRoles(moderatorRole);
+        userService.addUser(user);
+        userService.addUser(admin);
+        userService.addUser(moderator);
+    }
+}
